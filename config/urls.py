f36e8ef729fa203b5d3857032260fa42caec417f
Mokgadi Rasekgala <rasekgalam@gmail.com>
2021-08-25 12:07:00 +0200

    add index view

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from config import views
 
 urlpatterns = [
     path('shops/', include('shops.urls')),
+    path('', views.index, name='index'),
     path('maintenance/', views.maintenance, name='maintenance'),
     path('admin/', admin.site.urls)
 ]
--- config/views.py
+++ config/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Index page')
+
 
 def maintenance(request):
     return render(request, 'config/maintenance.html')
